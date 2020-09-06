a7bf80474881d2e0b0d8d7f111b51bb8cbb436b1
JaviB10 <javi4195@gmail.com>
2020-09-06 17:41:48 -0300

    Commit Inicial

---
+++ CERAMICOS.py
@@ -0,0 +1,9 @@
+#CONSIGNA DEL PROGRAMA
+#Se requiere un programa para un negocio en el cual se soliciten los siguientes datos:
+#Largo y Ancho de una habitacion
+#Largo y Ancho de un ceramico elegido
+#Precio por caja (sabiendo que por cada caja vienen 10 ceramicos)
+#Para obtener:
+#Cantida de ceramicos que se necesitan
+#Cantidad de cajas a comprar
+#Precio a pagar
