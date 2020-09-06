09f0d94740587313ede7547c0237d73981a05a2e
JaviB10 <javi4195@gmail.com>
2020-09-06 18:02:35 -0300

    Ingreso de datos solicitados para calcular

--- CERAMICOS.py
+++ CERAMICOS.py
@@ -7,3 +7,22 @@
 #Cantida de ceramicos que se necesitan
 #Cantidad de cajas a comprar
 #Precio a pagar
+
+
+#Primero preguntamos si se desea utilizar el programa para calcular
+NEGOCIO = str(input("Si desea ingresar los datos ponga S, de lo contrario ponga N"))
+
+while NEGOCIO == "S":
+
+    #Solicitamos en largo y ancho de la habitacion
+    LARGOHAB = float(input("Ingrese el largo de la habitacion: "))
+    ANCHOHAB = float(input("Ingrese el ancho de la habitacion: "))
+
+    #Ahora solicitamos el largo y ancho del ceramico que elegimos
+    LARGOCER = float(input("Ingrese el largo del ceramico: "))
+    ANCHOCER = float(input("Ingrese el ancho del ceramico: "))
+
+    #Ahora solicitaremos el precio por caja de los ceramicos elegidos
+    PRECIOCER = float(input("Ingrese el precio por caja de los ceramicos elegidos: "))
+
+    #En esta primera parte solicitamos los datos para realizar los calculos
